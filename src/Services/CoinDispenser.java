3c178dd853cd41f5e6e31b3001d94fa1038874fc
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-12-05 18:00:14 +0300

    save 05/12/23

--- src/Services/CoinDispenser.java
+++ src/Services/CoinDispenser.java
@@ -13,12 +13,12 @@ public class CoinDispenser {
 
     
 
-  public int getSumm(int insertCoin, int outCoin){
+  public int getSumm(int insertCoin, int outCoin) {
       int summCoin;
       summCoin=insertCoin+outCoin;
       return summCoin;
   } 
-  public int giveChahge(int summCoin, int price ){
+  public int giveChahge(int summCoin, int price ) {
       int change;
       if (price<summCoin){
           change=summCoin-price;
